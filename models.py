5ff25b54f12a65f983b0d153067b5e178e480a93
Ethan Reynolds <ethan.t.reynolds@gmail.com>
2023-02-23 18:30:43 -0700

    add user auth

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
+bcrypt = Bcrypt()
 db = SQLAlchemy()
 
 
@@ -12,6 +14,40 @@ class User(db.Model):
     username = db.Column(db.Text, nullable=False, unique=True)
     password = db.Column(db.Text, nullable=False)
     email = db.Column(db.Text, nullable=False, unique=True)
+
+    def __repr__(self):
+        return f"<User #{self.id}: {self.username}, {self.email}>"
+
+    @classmethod
+    def signup(cls, username, password, email):
+        """Sign up user. Hashes password and adds user to system."""
+
+        hashed_pw = bcrypt.generate_password_hash(password).decode('UTF-8')
+
+        user = User(username=username, email=email, password=hashed_pw)
+
+        db.session.add(user)
+        return user
+
+    @classmethod
+    def authenticate(cls, username, password):
+        """Find user with 'username' and 'password'.
+
+        This is a class method (call it on the class, not an individual user.)
+        It searches for a user whose password hash matches this password
+        and, if it finds such a user, returns that user object.
+
+        If can't find matching user (or if password is wrong), returns False.
+        """
+
+        user = cls.query.filter_by(username=username).first()
+
+        if user:
+            is_auth = bcrypt.check_password_hash(user.password, password)
+            if is_auth:
+                return user
+
+        return False
 
 
 def connect_db(app):
